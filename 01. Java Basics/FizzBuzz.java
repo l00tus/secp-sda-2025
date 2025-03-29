3b8a83f019fd35da30f1559afb7e39da377ddb74
l00tus <86197322+l00tus@users.noreply.github.com>
2025-03-29 12:44:20 +0000

    feat: add solution with streams

--- 01. Java Basics/FizzBuzz.java
+++ 01. Java Basics/FizzBuzz.java
@@ -1,6 +1,8 @@
 // The program should iterate from 1 to 100 and print 'Fizz' for multiples of 3, 'Buzz' for multiples of 5,
 // 'FizzBuzz' for multiples of both 3 and 5, and the number itself for all other cases.“
 
+import java.util.stream.IntStream;
+
 public class FizzBuzz {
     public static void solve() {
         for(int i = 1 ; i <= 100 ; i++) {
@@ -15,7 +17,14 @@ public class FizzBuzz {
             }
         }
     }
+
+    public static void solveWithStreams() {
+        IntStream.rangeClosed(1, 100)
+                .mapToObj(number -> (number % 15 == 0) ? "FizzBuzz" : (number % 3 == 0) ? "Fizz" : (number % 5 == 0) ? "Buzz" : String.valueOf(number))
+                .forEach(System.out::println);
+    }
     public static void main(String[] args) {
         solve();
+        solveWithStreams();
     }
 }
